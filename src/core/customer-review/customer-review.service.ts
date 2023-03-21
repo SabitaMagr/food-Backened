0af414f87d397d407eae3a4db3164d7aacd20c0b
SabitaMagr <sabitathapa9864@gmail.com>
2023-03-21 23:09:38 +0545

    feat:customer

--- src/core/customer-review/customer-review.service.ts
+++ src/core/customer-review/customer-review.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Mode } from 'fs';
+import { Model } from 'mongoose';
 import { CreateCustomerReviewDto } from './dto/create-customer-review.dto';
 import { UpdateCustomerReviewDto } from './dto/update-customer-review.dto';
+import { CustomerReview } from './entities/customer-review.entity';
+
 
 @Injectable()
 export class CustomerReviewService {
-  create(createCustomerReviewDto: CreateCustomerReviewDto) {
-    return 'This action adds a new customerReview';
+  constructor(
+    @InjectModel(CustomerReview.name) private customerReviewModel: Model<CustomerReview>,
+  ) { }
+  async create(createCustomerReviewDto: CreateCustomerReviewDto) {
+    const customerReview = await this.customerReviewModel.create({
+      firstName: createCustomerReviewDto.firstName,
+      lastName: createCustomerReviewDto.lastName,
+      email: createCustomerReviewDto.email,
+      companyName: createCustomerReviewDto.companyName,
+      phoneNumber: createCustomerReviewDto.phoneNumber,
+      message: createCustomerReviewDto.message,
+    });
+    return customerReview;
+    // return 'This action adds a new customerReview';
   }
 
   findAll() {
-    return `This action returns all customerReview`;
+    return this.customerReviewModel.find({}, { __v: 0 });
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} customerReview`;
+    return this.customerReviewModel.findOne({ _id: id });
   }
 
   update(id: string, updateCustomerReviewDto: UpdateCustomerReviewDto) {
-    return `This action updates a #${id} customerReview`;
+    return this.customerReviewModel.updateOne({ _id: id }), updateCustomerReviewDto;
   }
 
   remove(id: string) {
-    return `This action removes a #${id} customerReview`;
+    return this.customerReviewModel.deleteOne({ _id: id });
   }
 }
+
+
